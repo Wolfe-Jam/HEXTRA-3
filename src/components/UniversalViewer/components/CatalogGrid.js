2ea96af8a04829f51f9400f9c068e5c775c53be3
Wolfe-Jam <wolfejam2020@gmail.com>
2025-01-22 02:56:23 -0500

    feat: Add sort dropdown menu to Color Catalog with four sorting options

--- src/components/UniversalViewer/components/CatalogGrid.js
+++ src/components/UniversalViewer/components/CatalogGrid.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import styled from 'styled-components';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Select, MenuItem, FormControl } from '@mui/material';
 import useColorStore from '../../../state/ColorState';
 import { CatalogSwatch, SwatchLabel } from '../styles/SwatchStyles';
+import { sortByName, sortByFamily, sortByPopularity } from '../../../data/catalogs/sortMethods';
 
 const GridContainer = styled(Box)`
   display: grid;
@@ -20,18 +21,48 @@ const GridContainer = styled(Box)`
   }
 `;
 
+const HeaderContainer = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 16px 8px;
+`;
+
 const Title = styled(Typography)`
   font-family: 'Inter', sans-serif;
   font-size: 16px;
   font-weight: 700;
   color: #333;
-  padding-left: 16px;
-  margin: 16px 0 8px;
   text-transform: uppercase;
+`;
+
+const SortSelect = styled(Select)`
+  min-width: 150px;
+  height: 32px;
+  margin-left: 16px;
+  
+  .MuiSelect-select {
+    padding: 6px 12px;
+    font-size: 14px;
+  }
 `;
 
 const CatalogGrid = ({ colors, showHex = true }) => {
   const { currentColor, updateColorState } = useColorStore();
+  const [sortMethod, setSortMethod] = useState('default');
+
+  const sortedColors = useMemo(() => {
+    switch (sortMethod) {
+      case 'alphabetical':
+        return sortByName(colors);
+      case 'family':
+        return sortByFamily(colors);
+      case 'popularity':
+        return sortByPopularity(colors);
+      default:
+        return colors; // Default Printify order
+    }
+  }, [colors, sortMethod]);
 
   const handleColorSelect = (color) => {
     const hexValue = color.hex.startsWith('#') ? color.hex : `#${color.hex}`;
@@ -40,15 +71,30 @@ const CatalogGrid = ({ colors, showHex = true }) => {
 
   return (
     <>
-      <Title>COLOR CATALOG</Title>
+      <HeaderContainer>
+        <Title>COLOR CATALOG</Title>
+        <FormControl size="small">
+          <SortSelect
+            value={sortMethod}
+            onChange={(e) => setSortMethod(e.target.value)}
+            variant="outlined"
+          >
+            <MenuItem value="default">Printify Order (1-63)</MenuItem>
+            <MenuItem value="alphabetical">A-Z by Name</MenuItem>
+            <MenuItem value="family">Color Families</MenuItem>
+            <MenuItem value="popularity">Most Popular</MenuItem>
+          </SortSelect>
+        </FormControl>
+      </HeaderContainer>
       <GridContainer>
-        {colors.map((color) => (
+        {sortedColors.map((color) => (
           <Box key={color.hex} sx={{ position: 'relative' }}>
             <CatalogSwatch 
               color={color.hex}
               onClick={() => handleColorSelect(color)}
               className={color.hex.toUpperCase() === currentColor.toUpperCase() ? 'selected' : ''}
             >
+              {showHex && <SwatchLabel>{color.hex}</SwatchLabel>}
               <SwatchLabel>
                 {color.name}
               </SwatchLabel>
