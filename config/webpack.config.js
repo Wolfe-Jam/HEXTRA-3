054f517670b1d45cda8ad72109917783d7d6fa28
Wolfe-Jam <wolfejam2020@gmail.com>
2025-01-28 10:33:28 -0500

    Add import-meta-resolve and restore needed deps

---
+++ config/webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+
+module.exports = function override(config, env) {
+    // Add fallbacks for node modules that Jimp depends on
+    config.resolve.fallback = {
+        fs: false,
+        path: require.resolve('path-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer/'),
+        util: require.resolve('util/'),
+        assert: require.resolve('assert/'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        url: require.resolve('url/'),
+        querystring: require.resolve('querystring-es3'),
+        zlib: require.resolve('browserify-zlib'),
+        crypto: require.resolve('crypto-browserify'),
+    };
+
+    // Add buffer to plugins
+    config.plugins.push(
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+            process: 'process/browser',
+        })
+    );
+
+    return config;
+};
