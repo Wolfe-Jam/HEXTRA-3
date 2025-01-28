80f32d9c829873868bc1ee00fc655615b24e2195
Wolfe-Jam <wolfejam2020@gmail.com>
2025-01-27 19:46:24 -0500

    fix: simplify webpack config and add missing polyfills

--- next.config.js
+++ next.config.js
@@ -8,33 +8,23 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Don't attempt to polyfill these on the server
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-        stream: require.resolve('stream-browserify'),
-        crypto: require.resolve('crypto-browserify'),
-        http: require.resolve('stream-http'),
-        https: require.resolve('https-browserify'),
-        util: require.resolve('util/'),
-        zlib: require.resolve('browserify-zlib'),
-        buffer: require.resolve('buffer/'),
-        process: require.resolve('process/browser'),
-        querystring: require.resolve('querystring-es3'),
-      };
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false,
+      canvas: false,
+    };
 
-      config.plugins = [
-        ...config.plugins,
-        // Add polyfills for process and buffer
-        new webpack.ProvidePlugin({
-          process: 'process/browser',
-          Buffer: ['buffer', 'Buffer'],
-        }),
-      ];
-    }
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer'],
+      })
+    );
+
     return config;
   },
 }
