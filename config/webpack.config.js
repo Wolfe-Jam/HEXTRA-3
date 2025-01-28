51c5be7dde9c480e8de18cf77c42f4dd72540ec4
Wolfe-Jam <wolfejam2020@gmail.com>
2025-01-28 10:41:26 -0500

    Update webpack config with proper TerserPlugin config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,6 @@
 const webpack = require('webpack');
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = function override(config, env) {
     // Add fallbacks for node modules that Jimp depends on
@@ -24,6 +26,33 @@ module.exports = function override(config, env) {
             process: 'process/browser',
         })
     );
+
+    // Update optimization
+    config.optimization = {
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    parse: {
+                        ecma: 8,
+                    },
+                    compress: {
+                        ecma: 5,
+                        warnings: false,
+                        comparisons: false,
+                        inline: 2,
+                    },
+                    mangle: {
+                        safari10: true,
+                    },
+                    output: {
+                        ecma: 5,
+                        comments: false,
+                        ascii_only: true,
+                    },
+                },
+            }),
+        ],
+    };
 
     return config;
 };
