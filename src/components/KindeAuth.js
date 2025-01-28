94086581b1602388d99de2c9d2b64bbae9e6049b
Wolfe-Jam <wolfejam2020@gmail.com>
2025-01-28 18:12:21 -0500

    Update: Use KINDE_ prefixed environment variables

--- src/components/KindeAuth.js
+++ src/components/KindeAuth.js
@@ -6,10 +6,9 @@ export default function KindeAuth({ children }) {
   console.log('Environment:', {
     nodeEnv: process.env.NODE_ENV,
     clientId: process.env.KINDE_CLIENT_ID,
-    issuerUrl: process.env.KINDE_ISSUER_URL,
-    siteUrl: process.env.KINDE_SITE_URL,
-    postLoginRedirectUrl: process.env.KINDE_POST_LOGIN_REDIRECT_URL,
-    postLogoutRedirectUrl: process.env.KINDE_POST_LOGOUT_REDIRECT_URL
+    domain: process.env.KINDE_ISSUER_URL,
+    redirectUri: process.env.KINDE_POST_LOGIN_REDIRECT_URL,
+    logoutUri: process.env.KINDE_POST_LOGOUT_REDIRECT_URL
   });
 
   const config = {
