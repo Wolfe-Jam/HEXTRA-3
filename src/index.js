ff1dbce8b81e09f4d39bb20f9d0110451e8c1859
Wolfe-Jam <wolfejam2020@gmail.com>
2025-01-28 11:06:11 -0500

    Move polyfills to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import { Buffer } from 'buffer';
+import process from 'process';
+
+window.Buffer = Buffer;
+window.process = process;
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './setupPolyfills';
