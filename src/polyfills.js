7a07b41c06f7c8bb3e077a0b99aa8a95fe34cee6
Wolfe-Jam <wolfejam2020@gmail.com>
2025-01-28 11:36:01 -0500

    Update dependencies and polyfills to resolve vulnerabilities

--- src/polyfills.js
+++ src/polyfills.js
@@ -12,6 +12,10 @@ if (typeof window !== 'undefined') {
   window.Buffer = window.Buffer || Buffer;
   window.process = window.process || process;
   window.global = window;
+  
+  // Use URLSearchParams instead of querystring
+  window.URLSearchParams = window.URLSearchParams || URLSearchParams;
+  window.URL = window.URL || URL;
 }
 
 // Additional polyfills for specific features
@@ -21,6 +25,8 @@ import 'core-js/features/object/from-entries';
 import 'core-js/features/promise/finally';
 import 'core-js/features/string/pad-start';
 import 'core-js/features/string/pad-end';
+import 'core-js/features/url';
+import 'core-js/features/url-search-params';
 
 // Stream polyfills
 import 'stream-browserify';
