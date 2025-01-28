4281a03152145e8595ec5f4e5a628aaceed9b482
Wolfe-Jam <wolfejam2020@gmail.com>
2025-01-27 19:39:50 -0500

    fix: add Node polyfills for Jimp

--- next.config.js
+++ next.config.js
@@ -6,20 +6,33 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true
   },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      stream: require.resolve('stream-browserify'),
-      crypto: require.resolve('crypto-browserify'),
-      http: require.resolve('stream-http'),
-      https: require.resolve('https-browserify'),
-      util: require.resolve('util/'),
-      zlib: require.resolve('browserify-zlib'),
-      path: require.resolve('path-browserify'),
-      buffer: require.resolve('buffer/'),
-      process: require.resolve('process/browser'),
-      querystring: require.resolve('querystring-es3'),
-    };
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Don't attempt to polyfill these on the server
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        stream: require.resolve('stream-browserify'),
+        crypto: require.resolve('crypto-browserify'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        util: require.resolve('util/'),
+        zlib: require.resolve('browserify-zlib'),
+        buffer: require.resolve('buffer/'),
+        process: require.resolve('process/browser'),
+        querystring: require.resolve('querystring-es3'),
+      };
+
+      config.plugins = [
+        ...config.plugins,
+        // Add polyfills for process and buffer
+        new config.webpack.ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer'],
+        }),
+      ];
+    }
     return config;
   },
 }
