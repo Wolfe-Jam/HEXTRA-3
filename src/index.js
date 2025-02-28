dcdab1f1fa6e32192eccfb0bd3760b116bd6ca96
Wolfe-Jam <wolfejam2020@gmail.com>
2025-02-28 08:40:49 -0500

    Update theme and application entry point: Add new color variables and improve app initialization

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import './polyfills';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import themeManager from './theme';
 import KindeAuth from './components/KindeAuth';
+import { ThemeProvider } from './context/ThemeContext';
 
 console.log('App: Starting initialization...');
 const root = ReactDOM.createRoot(document.getElementById('root'));
-themeManager.init();
 
 root.render(
   <React.StrictMode>
-    <KindeAuth />
+    <ThemeProvider>
+      <KindeAuth />
+    </ThemeProvider>
   </React.StrictMode>
 );
